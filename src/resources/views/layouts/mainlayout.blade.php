487a3cf07e92df554e5dac6dd5d8dd86715aa8c0
Miroslav Novák <xnovak1k@stud.fit.vutbr.cz>
2017-05-03 15:49:17 +0100

    issue 17, 18

--- src/resources/views/layouts/mainlayout.blade.php
+++ src/resources/views/layouts/mainlayout.blade.php
@@ -10,8 +10,15 @@
 
     <title>@yield('title')</title>
 
+    <!-- Material Design fonts -->
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Roboto:300,400,500,700">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/icon?family=Material+Icons">
+
     <!-- Bootstrap Core CSS -->
     <link href="/css/bootstrap.min.css" rel="stylesheet">
+
+    {{--<link rel="stylesheet" type="text/css" href="/css/bootstrap-material-design.min.css">--}}
+    {{--<link rel="stylesheet" type="text/css" href="/css/ripples.min.css">--}}
 
     <!-- Bootstrap core CSS -->
 
@@ -91,6 +98,7 @@
         <!-- Navigation -->
         <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
             <div class="navbar-header">
+                <img class="navbar-brand" src="/img/testos-logo.png" style="padding: 5px 5px;">
                 <a class="navbar-brand" href="#">Testos</a>
             </div>
 
@@ -171,6 +179,11 @@
     {{-- </div> --}}
 
 </body>
-
-
+{{--<script src="/js/material.min.js"></script>--}}
+{{--<script src="/js/ripples.min.js"></script>--}}
+{{--<script>--}}
+    {{--$(document).ready(function() {--}}
+        {{--$.material.init()--}}
+        {{--})--}}
+{{--</script>--}}
 </html>
--- src/resources/views/library/librarySidemenu.blade.php
+++ src/resources/views/library/librarySidemenu.blade.php
@@ -4,10 +4,10 @@
 
 
 <a href="{{ url('library')}}" style="{{ Request::is('library') ? 'color:white' : '' }}" >All tests</a>
-<a id="newSuite" href="{{ url('library/testsuite/create')}}"style="{{ Request::is('library/testsuite/create') ? 'color:white' : '' }}"><span class="fa fa-plus  fa-fw"></span> New test suite</i></a>
 <hr />
 @if (isset($testSuites))
     @foreach ($testSuites as $testSuite)
         <a href="{{ url("library/filter/$testSuite->TestSuite_id")}}" style="{{ Request::is("library/filter/$testSuite->TestSuite_id") ? 'color:white' : '' }}">{{ $testSuite->Name }}</a>
     @endforeach
 @endif
+<a id="newSuite" href="{{ url('library/testsuite/create')}}"style="{{ Request::is('library/testsuite/create') ? 'color:white' : '' }}"><span class="fa fa-plus  fa-fw"></span> New test suite</i></a>
